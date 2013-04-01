a70b4b26a0c2ed2affb1b242fa40a8ac1c19d14d
Sridhar M <navitas2013@yahoo.com>
2013-04-01 12:12:37 +0530

    initial files

---
+++ src/main/java/com/wipro/atlassian/TestClass.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.wipro.atlassian;
+
+/**
+ * @author user
+ *
+ */
+public class TestClass {
+	public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+}
